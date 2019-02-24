a0c161879a1c15e8389dd411656a2956f11d161b
FOGOK <fogok.production@gmail.com>
2019-02-24 09:02:07 +0300

    copying if file not same

--- src/main/java/com/vionis/unityfastrunner/Markdown.java
+++ src/main/java/com/vionis/unityfastrunner/Markdown.java
@@ -5,6 +5,9 @@ import com.intellij.util.ui.UIUtil;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Markdown {
@@ -50,13 +53,18 @@ public class Markdown {
             File[] files = sourceLocation.listFiles();
 
             for (int i = 0; i < files.length; i++) {
+                progressIndicator.setFraction(i / (double)files.length * 0.9);
                 File file = files[i];
 
                 if (!file.getName().split("\\.")[1].equals("dll") && !file.getName().split("\\.")[1].equals("pdb"))
                     continue;
 
                 InputStream in = new FileInputStream(file);
-                OutputStream out = new FileOutputStream(targetLocation+"/"+file.getName());
+                String outPath = targetLocation+"/"+file.getName();
+                OutputStream out = new FileOutputStream(outPath);
+
+                if (sameContent(file.toPath(), new File(outPath).toPath()))
+                    continue;
 
                 // Copy the bits from input stream to output stream
                 byte[] buf = new byte[1024];
@@ -66,9 +74,31 @@ public class Markdown {
                 }
                 in.close();
                 out.close();
-                progressIndicator.setFraction(i / (double)files.length * 0.9);
             }
         }
+    }
+
+    static boolean sameContent(Path file1, Path file2) throws IOException {
+        final long size = Files.size(file1);
+        if (size != Files.size(file2))
+            return false;
+
+        if (size < 4096)
+            return Arrays.equals(Files.readAllBytes(file1), Files.readAllBytes(file2));
+
+        try (InputStream is1 = Files.newInputStream(file1);
+             InputStream is2 = Files.newInputStream(file2)) {
+            // Compare byte-by-byte.
+            // Note that this can be sped up drastically by reading large chunks
+            // (e.g. 16 KBs) but care must be taken as InputStream.read(byte[])
+            // does not neccessarily read a whole array!
+            int data;
+            while ((data = is1.read()) != -1)
+                if (data != is2.read())
+                    return false;
+        }
+
+        return true;
     }
 
 
